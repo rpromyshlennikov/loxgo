6a5d69caced0b8c04139a35fbd584975243e02bb
Rodion Promyshlennikov <rodion.prm@gmail.com>
2024-03-09 18:46:51 +0300

    Get rid of pointers in AstPrinter receivers
    AstPrinter has no state, so passing it as pointer does not seems useful.

--- pkg/parser/ast_printer.go
+++ pkg/parser/ast_printer.go
@@ -4,33 +4,32 @@ import (
 	"fmt"
 )
 
-type AstPrinter struct {
-}
+type AstPrinter struct{}
 
-func (p *AstPrinter) Sprint(expr Expr) string {
+func (p AstPrinter) Sprint(expr Expr) string {
 	return expr.accept(p).(string)
 }
 
-func (p *AstPrinter) visitUnary(unary *Unary) any {
+func (p AstPrinter) visitUnary(unary *Unary) any {
 	return p.parenthesize(unary.operator.Lexeme(), unary.right)
 }
 
-func (p *AstPrinter) visitBinary(binary *Binary) any {
+func (p AstPrinter) visitBinary(binary *Binary) any {
 	return p.parenthesize(binary.operator.Lexeme(), binary.left, binary.right)
 }
 
-func (p *AstPrinter) visitLiteral(literal *Literal) any {
+func (p AstPrinter) visitLiteral(literal *Literal) any {
 	if literal.value == nil {
 		return "nil"
 	}
 	return fmt.Sprint(literal.value)
 }
 
-func (p *AstPrinter) visitGrouping(grouping *Grouping) any {
+func (p AstPrinter) visitGrouping(grouping *Grouping) any {
 	return p.parenthesize("group", grouping.expression)
 }
 
-func (p *AstPrinter) parenthesize(name string, exprs ...Expr) any {
+func (p AstPrinter) parenthesize(name string, exprs ...Expr) any {
 	str := "(" + name
 	for _, expr := range exprs {
 		str += " " + expr.accept(p).(string)
