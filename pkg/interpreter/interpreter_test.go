4cfd5ffb4ef1173e56813fa084037088079c9906
Rodion Promyshlennikov <rodion.prm@gmail.com>
2024-03-13 23:41:14 +0300

    Add several interpreter tests
    There were no negative tests, so cover interpreter better.

--- pkg/interpreter/interpreter_test.go
+++ pkg/interpreter/interpreter_test.go
@@ -111,4 +111,84 @@ func TestInterpreter_Interpret(t *testing.T) {
 			t.Errorf("Interpret() = %v, want %v, ast %s", got, want, pprinter.Sprint(expr))
 		}
 	})
+
+	t.Run("Comparisons between nils works also fine", func(t *testing.T) {
+		// arrange
+		scnr := scanner.NewScanner("nil == nil", nil)
+		prsr := parser.NewParser(scnr.ScanTokens(), nil)
+		expr := prsr.Parse()
+		interp := Interpreter{}
+
+		// act
+		got, err := interp.Interpret(expr)
+
+		// assert
+		if err != nil {
+			t.Errorf("Interpret() return error: %s, but shouldn't", err)
+		}
+		want := "true"
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Interpret() = %v, want %v, ast %s", got, want, pprinter.Sprint(expr))
+		}
+	})
+
+	t.Run("Cannot interpret plus operator between string and number", func(t *testing.T) {
+		// arrange
+		scnr := scanner.NewScanner(`2 + "foo"`, nil)
+		prsr := parser.NewParser(scnr.ScanTokens(), nil)
+		expr := prsr.Parse()
+		interp := Interpreter{}
+
+		// act
+		_, err := interp.Interpret(expr)
+
+		// assert
+		wantErr := `Runtime error: "invalid type for operator '+' given, must be numbers or strings." at token: {PLUS + <nil> 1}`
+		if err == nil {
+			t.Errorf("Interpret() did not return error: %s, but should", wantErr)
+		}
+		if !reflect.DeepEqual(err.Error(), wantErr) {
+			t.Errorf("Interpret() error = %s, want error %s, ast %s", err.Error(), wantErr, pprinter.Sprint(expr))
+		}
+	})
+
+	t.Run("Cannot interpret multiply operator between string and number", func(t *testing.T) {
+		// arrange
+		scnr := scanner.NewScanner(`2 * "foo"`, nil)
+		prsr := parser.NewParser(scnr.ScanTokens(), nil)
+		expr := prsr.Parse()
+		interp := Interpreter{}
+
+		// act
+		_, err := interp.Interpret(expr)
+
+		// assert
+		wantErr := `Runtime error: "invalid type for operator STAR given, must be number." at token: {STAR * <nil> 1}`
+		if err == nil {
+			t.Errorf("Interpret() did not return error: %s, but should", wantErr)
+		}
+		if !reflect.DeepEqual(err.Error(), wantErr) {
+			t.Errorf("Interpret() error = %s, want error %s, ast %s", err.Error(), wantErr, pprinter.Sprint(expr))
+		}
+	})
+
+	t.Run("Cannot interpret nil expression (empty sources)", func(t *testing.T) {
+		// arrange
+		scnr := scanner.NewScanner("", nil)
+		prsr := parser.NewParser(scnr.ScanTokens(), nil)
+		expr := prsr.Parse()
+		interp := Interpreter{}
+
+		// act
+		_, err := interp.Interpret(expr)
+
+		// assert
+		wantErr := `Runtime error: "no expression given" at token: {EOF  <nil> 0}`
+		if err == nil {
+			t.Errorf("Interpret() did not return error: %s, but should", wantErr)
+		}
+		if !reflect.DeepEqual(err.Error(), wantErr) {
+			t.Errorf("Interpret() error = %s, want error %s, ast %s", err.Error(), wantErr, pprinter.Sprint(expr))
+		}
+	})
 }
