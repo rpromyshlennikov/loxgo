bd3ca71671a1dee0071fa91f53930a7d99bbeeca
Rodion Promyshlennikov <rodion.prm@gmail.com>
2024-03-24 22:34:35 +0300

    Prepare AST generator for statements
    There were some hardcoded values for Expressions not allowing
    adding of Statements and other AST tree node types.

--- pkg/parser/ast/expr.go
+++ pkg/parser/ast/expr.go
@@ -4,14 +4,11 @@ import (
 	"github.com/rpromyshlennikov/lox_tree_walk_interpretator/pkg/scanner"
 )
 
-type Token = scanner.Token
-type TokenType = scanner.TokenType
-
 type Expr interface {
-	Accept(visitor Visitor) any
+	Accept(visitor VisitorExpr) any
 }
 
-type Visitor interface {
+type VisitorExpr interface {
 	VisitBinary(*Binary) any
 	VisitGrouping(*Grouping) any
 	VisitLiteral(*Literal) any
@@ -22,12 +19,12 @@ type Binary struct {
 	// Left field.
 	Left Expr
 	// Operator field.
-	Operator Token
+	Operator scanner.Token
 	// Right field.
 	Right Expr
 }
 
-func NewBinary(left Expr, operator Token, right Expr) *Binary {
+func NewBinary(left Expr, operator scanner.Token, right Expr) *Binary {
 	this := Binary{}
 	this.Left = left
 	this.Operator = operator
@@ -35,7 +32,7 @@ func NewBinary(left Expr, operator Token, right Expr) *Binary {
 	return &this
 }
 
-func (b *Binary) Accept(visitor Visitor) any {
+func (b *Binary) Accept(visitor VisitorExpr) any {
 	return visitor.VisitBinary(b)
 }
 
@@ -50,7 +47,7 @@ func NewGrouping(expression Expr) *Grouping {
 	return &this
 }
 
-func (g *Grouping) Accept(visitor Visitor) any {
+func (g *Grouping) Accept(visitor VisitorExpr) any {
 	return visitor.VisitGrouping(g)
 }
 
@@ -65,24 +62,24 @@ func NewLiteral(value any) *Literal {
 	return &this
 }
 
-func (l *Literal) Accept(visitor Visitor) any {
+func (l *Literal) Accept(visitor VisitorExpr) any {
 	return visitor.VisitLiteral(l)
 }
 
 type Unary struct {
 	// Operator field.
-	Operator Token
+	Operator scanner.Token
 	// Right field.
 	Right Expr
 }
 
-func NewUnary(operator Token, right Expr) *Unary {
+func NewUnary(operator scanner.Token, right Expr) *Unary {
 	this := Unary{}
 	this.Operator = operator
 	this.Right = right
 	return &this
 }
 
-func (u *Unary) Accept(visitor Visitor) any {
+func (u *Unary) Accept(visitor VisitorExpr) any {
 	return visitor.VisitUnary(u)
 }
--- pkg/tools/ast_gen.go
+++ pkg/tools/ast_gen.go
@@ -1,41 +1,33 @@
 package tools
 
 import (
+	"fmt"
 	"os"
 	"path"
 	"strings"
 )
 
-func DefineAst(outputDir string, baseName string, types []string) {
-	absPath := path.Join(outputDir, strings.ToLower(baseName)+".go")
+func DefineAst(outputDir string, base string, types []string) {
+	names := strings.Split(base, ",")
+	absPath := path.Join(outputDir, strings.ToLower(names[0])+".go")
 	file, err := os.Create(absPath)
 	if err != nil {
 		panic(err)
 	}
 
 	strBuilder := strings.Builder{}
-	_, err = strBuilder.WriteString(`package ast
-
-import (
-	"github.com/rpromyshlennikov/lox_tree_walk_interpretator/pkg/scanner"
-)
-
-type Token = scanner.Token
-type TokenType = scanner.TokenType
-
-type Expr interface {
-	Accept(visitor Visitor) any
-}
-`)
+	_, err = strBuilder.WriteString("package ast\n")
 	if err != nil {
 		panic(err)
 	}
 
-	defineVisitor(&strBuilder, types)
+	defineImports(&strBuilder, names, types)
+
+	defineVisitor(&strBuilder, names, types)
 
 	// The AST classes.
 	for _, kind := range types {
-		defineType(&strBuilder, kind)
+		defineType(&strBuilder, names, kind)
 	}
 
 	n, err := file.WriteString(strBuilder.String())
@@ -47,9 +39,43 @@ type Expr interface {
 	}
 }
 
-func defineVisitor(builder *strings.Builder, types []string) {
-	// Producing Visitor interface.
-	_, err := builder.WriteString("\ntype Visitor interface {\n")
+func defineImports(builder *strings.Builder, names []string, types []string) {
+	_, baseReturnType := baseNames(names)
+	usedTypes := append([]string{baseReturnType}, types...)
+	addImport := false
+	for _, usedType := range usedTypes {
+		if strings.Contains(usedType, "scanner.") {
+			addImport = true
+		}
+
+	}
+	if !addImport {
+		return
+	}
+	_, err := builder.WriteString(`
+import (
+	"github.com/rpromyshlennikov/lox_tree_walk_interpretator/pkg/scanner"
+)
+`)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func defineVisitor(builder *strings.Builder, names []string, types []string) {
+	baseName, baseReturnType := baseNames(names)
+	// Producing AST Node and Visitor interfaces.
+	_, err := builder.WriteString(
+		fmt.Sprintf(`
+type %s interface {
+	Accept(visitor Visitor%s)%s
+}
+
+type Visitor%s interface {
+`,
+			baseName, baseName, baseReturnType, baseName,
+		),
+	)
 	if err != nil {
 		panic(err)
 	}
@@ -58,7 +84,11 @@ func defineVisitor(builder *strings.Builder, types []string) {
 	for _, kind := range types {
 		className, _ := classProps(kind)
 		_, err = builder.WriteString(
-			"\tVisit" + className + "(*" + className + ") any\n")
+			fmt.Sprintf(
+				"\tVisit%s(*%s)%s\n",
+				className, className, baseReturnType,
+			),
+		)
 		if err != nil {
 			panic(err)
 		}
@@ -74,9 +104,10 @@ func defineVisitor(builder *strings.Builder, types []string) {
 // TODO: refactor to several private methods or use templates after book will be completed.
 //
 //gocyclo:ignore
-func defineType(builder *strings.Builder, kind string) {
+func defineType(builder *strings.Builder, names []string, kind string) {
 	className, fieldsList := classProps(kind)
 	fields := strings.Split(fieldsList, ", ")
+	baseName, baseReturnType := baseNames(names)
 
 	// Producing type for Expression.
 	_, err := builder.WriteString("\ntype " + className + " struct {\n")
@@ -135,15 +166,30 @@ func defineType(builder *strings.Builder, kind string) {
 		panic(err)
 	}
 
+	returnStr := ""
+	if baseReturnType != "" {
+		returnStr = "return "
+	}
 	// Producing accept method.
 	_, err = builder.WriteString(
-		"func (" + strings.ToLower(className[:1]) + " *" + className + ") Accept(visitor Visitor) any {\n" +
-			"\treturn visitor.Visit" + className + "(" + strings.ToLower(className[:1]) + ")\n" +
-			"}\n",
+		fmt.Sprintf(
+			"func (%s *%s) Accept(visitor Visitor%s)%s {\n"+
+				"\t%svisitor.Visit%s(%s)\n"+
+				"}\n",
+			strings.ToLower(className[:1]), className, baseName, baseReturnType, returnStr, className, strings.ToLower(className[:1]),
+		),
 	)
 	if err != nil {
 		panic(err)
 	}
+}
+func baseNames(names []string) (string, string) {
+	baseName := names[0]
+	baseReturnType := ""
+	if len(names) > 1 {
+		baseReturnType = " " + names[1]
+	}
+	return baseName, baseReturnType
 }
 
 func classProps(kind string) (string, string) {
